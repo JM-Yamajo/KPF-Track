39b13cfa52adf62171b6cc91a0496a9e493426d1
JM-Yamajo <jh31.irs@gmail.com>
2024-05-19 12:46:16 -0600

    Library update

--- deep_sort/linear_assignment.py
+++ deep_sort/linear_assignment.py
@@ -1,8 +1,8 @@
 # vim: expandtab:ts=4:sw=4
 from __future__ import absolute_import
 import numpy as np
-from sklearn.utils.linear_assignment_ import linear_assignment
-from . import kalman_filter
+from deep_sort import linear_assignment
+from deep_sort import kalman_filter
 
 
 INFTY_COST = 1e5
